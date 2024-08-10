5be0e3fd12c7c323e8e94fd56b2fab979ce21453
WahibaZ <wahiba.zaaimia@gmail.com>
2024-08-10 20:52:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import { Component } from 'react';
+import profile from './Images/profile.jpg';
+
+
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+ // `props` représente les propriétés reçues par la composante enfant
+  constructor(props) { 
+  // Appel du constructeur de la classe parente (`React.Component`) avec `props`
+    super(props);      
+
+    // Initialisation de l'état (state) spécifique à cette composante enfant
+    this.state = {
+      // Attribut propre à la composante enfant
+      person:{
+      fullName: 'My name', 
+      bio: 'A software developer with 10 years of experience',
+      imgSrc: profile,
+      profession: 'Software Developer'
+    },
+    shows: false, // Initialement, le profil ne s'affiche pas
+    timeElapsed: 0
+    };
+    
+  }
+  toggleShow=()=>{
+    
+    this.setState({ shows: !this.state.shows });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        timeElapsed: prevState.timeElapsed + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render(){
+    return(
+      <div className="per">
+
+        <button onClick={this.toggleShow}> {this.state.shows ? 'Hide Profile' : 'Show Profile'}</button>
+
+        {this.state.shows ? (
+       
+          <div >
+          <Card  style={{ width: '25rem' }}>
+          <Card.Img  variant="top" src={this.state.person.imgSrc} alt="Profile" />
+          <Card.Body >
+            <Card.Title >{this.state.person.fullName}</Card.Title>
+            <Card.Text>
+              {this.state.person.bio} <br/>
+              {this.state.person.profession}
+            </Card.Text>          
+          </Card.Body>
+        </Card>
+        </div>
+        ) : null}
+            
+            <h5>Time since component mounted: {this.state.timeElapsed} seconds</h5>
+   
+      
+  
+        
+      </div>
+      );
+  }
+ 
 }
 
 export default App;
+
+      
+ 
